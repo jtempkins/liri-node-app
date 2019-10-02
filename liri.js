c7c1f22d4e2a90c4e9326133fe2e13f1cdb716c8
Joy Tempkins <jtempkins@gmail.com>
2019-10-02 12:50:51 -0500

    final rendition

--- liri.js
+++ liri.js
@@ -5,7 +5,6 @@ var Spotify = require("node-spotify-api");
 var fs = require("fs");
 var moment = require("moment")
 
-// var request = require('request');
 var spotify = new Spotify(keys.spotify);
 
 
@@ -40,10 +39,6 @@ switch (command) {
 function mySpotify(data) {
 
   var thisSearch = data;
-  // var spotify = new Spotify(keys.spotify{
-  //   id: keys.spotify.id,
-  //   secret: keys.spotify.secret
-  // });
 
   var searchTerm = artistSongMovie || "ace of base The Sign";
 
@@ -52,7 +47,7 @@ function mySpotify(data) {
     console.log(data.tracks.items[3].preview_url);
     console.log(data.tracks.items[0].album.name);
     });
-};
+  
 
 function myMovie() {
 
@@ -60,43 +55,54 @@ function myMovie() {
 
   axios.get('http://www.omdbapi.com/?t=' + searchTerm + '&apikey=6e8b6a0').then
   (function(response) {
-    console.log("Title: " + response.data.Title);
-    console.log("Year: " + response.data.Year);
-    console.log("Rating: " + response.data.imdbRating);
-    console.log("Rotten: " + response.data.Ratings[1].Value);
-    console.log("Country: " + response.data.Country);
-    console.log("Language: " + response.data.Language);
-    console.log("Plot: " + response.data.Plot);
-    console.log("Actors: " + response.data.Actors);
-  })};
+      console .log("Title: " + response.data.Title);
+      console.log("Year: " + response.data.Year);
+      console.log("Rating: " + response.data.imdbRating);
+      console.log("Rotten: " + response.data.Ratings[1].Value);
+      console.log("Country: " + response.data.Country);
+      console.log("Language: " + response.data.Language);
+      console.log("Plot: " + response.data.Plot);
+      console.log("Actors: " + response.data.Actors);
+      };
+    
 
   function myConcert() {
 
     var searchTerm = artistSongMovie;
-  
-    axios.get('http://rest.bandsintown.com/artists/' + searchTerm + '/events?app_id=codingbootcamp').then
-    (function(response) {
-      console.log(response.data[0].venue.name);
-    console.log(response.data[0].venue.city);
-    console.log(moment(response.data[0].datetime).format("MM/DD/YYYY"));
-   })
-  };
+  console.log(searchTerm, "What did we get?"); 
+  axios.get('http://rest.bandsintown.com/artists/' + searchTerm + '/events?app_id=codingbootcamp').then
+  (function(response) {
+      console .log(response.data[0].venue.name || "info not available");
+    console.log(response.data[0].venue.city || "info not available");
+      console.log(moment(response.data[0].datetime).format("MM/DD/YYYY"));
+      )
+    ;
 
   function doFromInput(){
-    fs.readFile("random.txt", "utf8", function(error, data) {
-      if (error) {
-        return console.log(error);
-      }
-          console.log(data + "this data");
-  
-          // Then split it by commas (to make it more readable)
-      // var dataArr = data;
+  fs.readFile("random. txt", "utf8", function(error, data) {
+    if (error) { 
+      return console.log(error);
+      
       var dataArr = data.split(",");
-      if (dataArr[0] == "spotify-this-song");
-      console.log(dataArr[0]) 
+      console.log(dataArr);
+
+      if (dataArr[0] == "spotify-this-song")
       { 
-        var searchTerm = dataArr[1].trim().slice(1, -1);
-        mySpotify(searchTerm);
-      }
+      artistSongMovie = dataArr[1].trim();
+      mySpotify();
+    }
+
+    else if (dataArr[0] == "movie-this") {
+      artistSongMovie = dataArr[1].trim();
+      console.log(artistSongMovie, "show me the movie");
+      myMovie();
+    }
+
+    else if (dataArr[0] == "concert-this") {
+      artistSongMovie = dataArr[1].trim();
+      console.log(artistSongMovie, "show me the artist");
+      myConcert();
+    }
+
 
   })};
